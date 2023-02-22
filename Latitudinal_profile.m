d9e34d6c1bbba99c7bae7ca95f366e5c21cc422e
jcsunn <112635139+jcsunn@users.noreply.github.com>
2023-02-22 21:45:10 +0800

    Update Latitudinal_profile.m

--- Latitudinal_profile.m
+++ Latitudinal_profile.m
@@ -6,22 +6,26 @@ depth = ncread(filename, 'LEV');
 temp = ncread(filename, 'water_temp');
 
 % Select zonal section to plot
-lon_index = 120; % index of longitude to plot
-lat_range = [26, 30]; % latitude range to plot
+lon_range = [120, 135]; % index of latitude to plot
+lat_index = 26; % longitude range to plot
+depth_min = 0; % minimum longitude of ROI
+depth_max = 1000; % maximum longitude of ROI
 
 % Find nearest latitude indices to lat_range
-[~,lat_min_index] = min(abs(lat-lat_range(1)));
-[~,lat_max_index] = min(abs(lat-lat_range(2)));
+[~,lon_min_index] = min(abs(lon-lon_range(1)));
+[~,lon_max_index] = min(abs(lon-lon_range(2)));
+depth_idx = find(depth >= depth_min & depth <= depth_max);
 
 % Extract data for selected zonal section and latitude range
-temp_zonal = squeeze(temp(lon_index,lat_min_index:lat_max_index,:,1));
-depth_zonal = squeeze(depth);
+temp_zonal = squeeze(temp(lon_min_index:lon_max_index,lat_index,depth_idx,1));%lon,lat,depth,time
+depth_zonal = squeeze(depth(depth_idx));
 temp_zonal(temp_zonal<-1000) = nan; 
+
 % Create zonal profile plot
 figure;
-pcolor(lat(lat_min_index:lat_max_index), -depth_zonal, temp_zonal');
+pcolor(lon(lon_min_index:lon_max_index), -depth_zonal, temp_zonal');
 shading interp;
 colorbar;
-xlabel('Latitude');
+xlabel('Longitude');
 ylabel('Depth (m)');
-title(['Zonal Profile at Latitude ', num2str(lon_index)]);
+title(['Zonal Profile at Latitude ', num2str(lat_index)]);
